4a2821210b512dde9b0a80f9d542acb366fe11ba
Diego__Leite <diegoleite59986@gmail.com>
2024-11-19 22:20:43 -0300

    aaa

--- menu.js
+++ menu.js
@@ -14,3 +14,17 @@ menu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajuste de 50px para cabeçalhos fixos
+                behavior: 'smooth'
+            });
+        }
+    });
+});
